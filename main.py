1253ae5c7d07292f3bddc7d71fbeb7537cc01fb2
beervajoshiactowiz-netizen <beervajoshi.actowiz@gmail.com>
2026-03-27 17:36:34 +0530

    it first get all country urls from base url, and send them all into run function to process deeper, and store into db

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
+from parser import run
 from db import setup_database
-from phase1_parser import run as phase1
-from phase2_parser import run as phase2
+from config import BASE_URL, HEADERS
+from urllib.parse import urljoin
+import requests
+from lxml import html
+import time
 
-if __name__ == "__main__":
+def main():
     setup_database()
 
-    print("1 → Crawl URLs")
-    print("2 → Extract Data (Live Pipeline)")
+    # Get country URLs from homepage
+    res  = requests.get(BASE_URL, headers=HEADERS)
+    tree = html.fromstring(res.content)
 
-    choice = input("Enter choice: ")
+    seen         = set()
+    country_urls = []
 
-    if choice == "1":
-        phase1()
-    elif choice == "2":
-        phase2()
+    for link in tree.xpath('//a[contains(@title,"Postal Codes")]'):
+        href     = link.get("href")
+        full_url = urljoin(BASE_URL, href)
+        path     = full_url.replace(BASE_URL, "").strip("/")
+
+        # Only country level (no slash in path)
+        if "/" not in path and full_url not in seen:
+            seen.add(full_url)
+            country_urls.append(full_url)
+
+    print(f"Total countries: {len(country_urls)}")
+
+    # Start scraping
+    run(start_urls=country_urls)
+
+if __name__ == "__main__":
+    st=time.time()
+    main()
+    et=time.time()
+    print(et-st)
